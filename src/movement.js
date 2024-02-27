490850614ed08ed0fb7c31fbae753c49d5131e71
SlimRunner <dv.polygon@gmail.com>
2024-02-27 04:41:19 -0800

    implement mouse driven 1st person

--- src/movement.js
+++ src/movement.js
@@ -20,6 +20,20 @@ export class Walk_Movement extends Scene {
     // tiny-graphics, but it does not destroy intellisense by
     // unecessary levels of indirection.
 
+    this.consts = Object.freeze({
+      cardinal_dir: Object.freeze({
+        N: 1,
+        W: 2,
+        S: 4,
+        E: 8,
+        NW: 1 | 2,
+        NE: 1 | 8,
+        SW: 4 | 2,
+        SE: 4 | 8,
+        ALL: 1 | 2 | 4 | 8,
+      }),
+    });
+
     this.thrust = 0;
     this.speed = 0;
     this.position = Float3.create(0, 0, 0);
@@ -30,11 +44,10 @@ export class Walk_Movement extends Scene {
     this.compass = Vector3.create(0, 0, 0);
     this.up_axis = Vector3.create(0, 1, 0);
     this.walk_force = 22.0;
-    this.turning_force = 9.28;
-    this.speed_limit = 30;
-    this.turn_speed_limit = 3.0;
+    this.momentum_vector = vec3(0, 0, 0);
+
+    this.speed_limit = 20;
     this.speed_decay_factor = 0.8;
-    this.angular_decay_factor = 0.3;
 
     this.gravity = -40;
     this.min_height = 1.5;
@@ -50,20 +63,31 @@ export class Walk_Movement extends Scene {
       h_angle: 0,
       v_angle: 0
     }
+  }
 
-    this.consts = Object.freeze({
-      cardinal_dir: Object.freeze({
-        N: 1,
-        W: 2,
-        S: 4,
-        E: 8,
-        NW: 1 | 2,
-        NE: 1 | 8,
-        SW: 4 | 2,
-        SE: 4 | 8,
-        ALL: 1 | 2 | 4 | 8,
-      }),
-    });
+  map_cardinal_to_vec(dir) {
+    const cardinal = this.consts.cardinal_dir;
+    let result = vec3(0, 0, 0);
+
+    if (dir & cardinal.N) {
+      result[0] += 1;
+    }
+    if (dir & cardinal.S) {
+      result[0] -= 1;
+    }
+    if (dir & cardinal.W) {
+      result[2] -= 1;
+    }
+    if (dir & cardinal.E) {
+      result[2] += 1;
+    }
+
+    result.normalize();
+    if (result[0] === result[0]) {
+      return result;
+    } else {
+      return null;
+    }
   }
 
   set_recipient(matrix_closure, inverse_closure, camera) {
@@ -93,20 +117,39 @@ export class Walk_Movement extends Scene {
         2 * (e.clientX - rect.left) / (rect.right - rect.left) - 1,
         2 * (e.clientY - rect.top) / (rect.bottom - rect.top) - 1
       );
-    // Set up mouse response.  The last one stops us from reacting if the mouse leaves the canvas:
-    document.addEventListener("mouseup", (e) => {
-      this.mouse.enabled = true;
-    });
+    const update_mouse = (e, rect = canvas.getBoundingClientRect()) => {
+      this.mouse.from_center[0] += 2 * e.movementX / (rect.right - rect.left);
+      this.mouse.from_center[1] = Math.max(
+        -1, Math.min(1, this.mouse.from_center[1] + e.movementY / (rect.bottom - rect.top))
+      );
+    }
     canvas.addEventListener("mousedown", (e) => {
       e.preventDefault();
-      this.mouse.from_center = mouse_position(e);
+      // code for interactivity here
     });
-    canvas.addEventListener("mousemove", (e) => {
+    document.addEventListener("mousemove", (e) => {
       e.preventDefault();
-      this.mouse.from_center = mouse_position(e);
+      if (document.pointerLockElement === canvas) {
+        update_mouse(e);
+      }
     });
     canvas.addEventListener("mouseout", (e) => {
       this.mouse.from_center.scale_by(0);
+    });
+
+    // capture mouse to element
+    canvas.addEventListener("click", (e) => {
+      canvas.requestPointerLock = canvas.requestPointerLock || canvas.mozRequestPointerLock;
+      canvas.requestPointerLock({
+        unadjustedMovement: true,
+      });
+    });
+
+    // exit pointer lock (browsers usually implement this)
+    document.addEventListener('keydown', (e) => {
+      if (e.key === 'Escape') {
+        document.exitPointerLock();
+      }
     });
   }
 
@@ -154,7 +197,9 @@ export class Walk_Movement extends Scene {
         this.dir_flag |= dir.N;
       },
       undefined,
-      () => (this.dir_flag &= dir.ALL ^ dir.N)
+      () => {
+        this.dir_flag &= dir.ALL ^ dir.N;
+      }
     );
     this.new_line();
     this.key_triggered_button(
@@ -164,7 +209,9 @@ export class Walk_Movement extends Scene {
         this.dir_flag |= dir.W;
       },
       undefined,
-      () => (this.dir_flag &= dir.ALL ^ dir.W)
+      () => {
+        this.dir_flag &= dir.ALL ^ dir.W;
+      }
     );
     this.key_triggered_button(
       "Back",
@@ -173,7 +220,9 @@ export class Walk_Movement extends Scene {
         this.dir_flag |= dir.S;
       },
       undefined,
-      () => (this.dir_flag &= dir.ALL ^ dir.S)
+      () => {
+        this.dir_flag &= dir.ALL ^ dir.S;
+      }
     );
     this.key_triggered_button(
       "Right",
@@ -182,7 +231,9 @@ export class Walk_Movement extends Scene {
         this.dir_flag |= dir.E;
       },
       undefined,
-      () => (this.dir_flag &= dir.ALL ^ dir.E)
+      () => {
+        this.dir_flag &= dir.ALL ^ dir.E;
+      }
     );
     this.new_line();
   }
@@ -197,36 +248,43 @@ export class Walk_Movement extends Scene {
 
     // desmos graph where I figured out the correct scaling for the mouse
     // https://www.desmos.com/calculator/bcgbn3fbzh
-    this.look_angle.h_angle = lerp(this.look_angle.h_angle, look_towards[0] * Math.PI * 0.5, 0.1);
-    this.look_angle.v_angle = lerp(this.look_angle.v_angle, look_towards[1] * Math.PI * 0.5, 0.1);
+    // this introduces a slight delay in the camera movement
+    // but it makes the camera movement feel more natural
+    this.look_angle.v_angle = lerp(this.look_angle.v_angle, look_towards[1] * Math.PI * 0.5, 0.8);
     look_around_matrix.post_multiply(Mat4.rotation(this.look_angle.v_angle, 1, 0, 0));
-    look_around_matrix.post_multiply(Mat4.rotation(this.look_angle.h_angle, 0, 1, 0));
 
     const dir = this.consts.cardinal_dir;
     const thrustforce = this.walk_force;
-    const torqueforce = this.turning_force;
     this.thrust = 0;
-    this.torque = 0;
+    
+    this.direction = this.mouse.from_center[0];
 
-    if (this.dir_flag & dir.N) {
-      this.thrust += thrustforce;
-    }
-    if (this.dir_flag & dir.S) {
-      this.thrust += -thrustforce;
-    }
-    if (this.dir_flag & dir.W) {
-      this.torque += -torqueforce;
-    }
-    if (this.dir_flag & dir.E) {
-      this.torque += torqueforce;
-    }
+    const heading_cos = Math.cos(this.direction);
+    const heading_sin = Math.sin(this.direction);
 
     const heading = Vector3.create(
-      Math.cos(this.direction),
+      heading_cos,
       0,
-      Math.sin(this.direction)
+      heading_sin
     );
     const airborne = this.height > this.min_height;
+    const walk_vector = this.map_cardinal_to_vec(this.dir_flag);
+
+    if (walk_vector !== null) {
+      this.thrust = thrustforce;
+    }
+
+    if (!airborne) {
+      if (walk_vector !== null) {
+        this.momentum_vector = vec3(
+          walk_vector[0] * heading_cos - walk_vector[2] * heading_sin,
+          0,
+          walk_vector[0] * heading_sin + walk_vector[2] * heading_cos
+        );
+      } else {
+        // this.momentum_vector = heading;
+      }
+    }
 
     if (!airborne && this.jump_thrust == 0) {
       this.fall_speed = 0;
@@ -239,20 +297,12 @@ export class Walk_Movement extends Scene {
       );
     }
 
-    this.angular_speed = min_abs(
-      this.angular_speed + this.torque * dt,
-      this.turn_speed_limit
-    );
-    if (!(this.dir_flag & (dir.W | dir.E))) {
-      this.angular_speed *= this.angular_decay_factor;
-    }
-    this.direction += this.angular_speed * dt;
-
     this.speed = min_abs(this.speed + this.thrust * dt, this.speed_limit);
-    if (!(this.dir_flag & (dir.N | dir.S)) && !airborne) {
+    if (!(this.dir_flag & (dir.ALL)) && !airborne) {
       this.speed *= this.speed_decay_factor;
     }
-    this.position.add_by(heading.times(this.speed * dt));
+    
+    this.position.add_by(this.momentum_vector.times(this.speed * dt));
     this.position[1] = this.height;
 
     state.set_camera(look_around_matrix.times(custom_look_at(this.position, heading, this.up_axis)));
